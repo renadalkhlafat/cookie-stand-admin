f2e9fbcc73042c84986a0da447a4027b1da5d9f0
renadalkhlafat <renadsalem8888@gmail.com>
2021-11-29 18:55:51 +0200

    cookie-stand-admin done

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,103 @@
 import Head from 'next/head'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [cookiesArray, setCoookiesArray] = useState([])
+
+  function submitHandler(e) {
+    e.preventDefault();
+    console.log();
+    const cookiesdata = {
+      maxCustomer: e.target.maxCustomer.value,
+      minCustomer: e.target.minCustomer.value,
+      avg: e.target.avg.value,
+      location: e.target.location.value,
+    }
+    setCoookiesArray(x => [...x, cookiesdata])
+  }
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Cookie Stand Admin</title>
       </Head>
-
+      <Header />
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <CookieForm onSubmit={submitHandler} />
+        <p class="text-center"> Report Table Commig Soon ...</p>
+        <Result cookiesArray={cookiesArray} />
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <Footer />
     </div>
   )
+}
+
+function Header() {
+  return (
+    <header class="w-full bg-green-400">
+      <h3 class="font-semibold text-black-100 md:text-3xl pt-4 pb-3 m-0"> Cookie Stand Admin</h3>
+    </header>
+  )
+}
+
+function Footer() {
+  return (
+    <footer className="flex flex-col  justify-center w-full bg-green-400 py-3">
+      <p > &copy; 2021</p>
+    </footer>
+  )
+}
+
+function CookieForm(props) {
+  return (
+    <form class="bg-green-300 bg-green-300 rounded-md" onSubmit={props.onSubmit}>
+      <div class="grid  gap-4 grid-rows-3">
+        <div class="flex flex-col pt-3 w-full  text-center font-semibold md:text-xl">Create Cookie Stand</div>
+        <div class="flex-2 m-3 ">
+          <lable class="px-4 py-2 text-black-50 font-semibold "> Location</lable>
+          <input type="text" class=" w-4/5 form-control " name="location" />
+        </div>
+        <div class="grid  gap-6 grid-cols-4 mb-5">
+          <div class="grid  gap-2 grid-rows-2 m-3 ">
+            <lable class="font-semibold"> Minimum Customers per Hour </lable>
+            <input type="text" class="form-control" name='maxCustomer' />
+          </div>
+          <div class="grid  gap-2 grid-rows-2 m-3">
+            <lable class="font-semibold" > Maximum Customers per Hour</lable>
+            <input type="text" class="form-control" name="minCustomer" />
+          </div>
+          <div class="grid  gap-2 grid-rows-2 m-3">
+            <lable class="font-semibold"> Average Cookies per Sale</lable>
+            <input type="text" class="form-control" name="avg" />
+          </div>
+          <div class="grid  gap-2 grid-rows-1 m-3">
+            <button class="font-semibold form-control bg-green-600" >Create</button>
+          </div>
+        </div>
+      </div>
+    </form>
+  )
+}
+
+function Result(props) {
+ console.log(props.cookiesArray);
+  if (props.cookiesArray.length == 0) {
+    return <p>Nothing Added</p>
+  } else {
+    return (
+      <p>{props.cookiesArray.map(cookie=>
+        <p>{`{ "Location":"${cookie.location}","minCustomers": "${cookie.minCustomer}","maxCustomers": "${cookie.minCustomer}" ,"avgCookies": "${cookie.avg}" }`}</p>
+        )}</p>
+     
+      // <div class="grid  gap-4 grid-rows-3">
+      //   {props.cookiesArray.map(cookie => {
+      //     console.log(cookie);
+
+      //    
+
+      //   })}
+      // </div>)
+    )
+  }
+
 }
