06b39e4c1713e892361856fd013c0479d491fa2c
renadalkhlafat <renadsalem8888@gmail.com>
2021-12-01 09:08:52 +0200

    structure

---
+++ components/RepostTable.js
@@ -0,0 +1,41 @@
+import { hours } from "../data";
+export default function ReportTable(props) {
+  console.log(props.cookiesArray);
+  if (props.cookiesArray.length == 0) {
+    return <h2 className="mt-4 md:text-2xl">No Cookie Stands Available</h2>
+  } else {
+    return (
+      <table className="mt-4 bg-green-500 border border-separate border-green-800 rounded-md table-fixed ">
+        <thead  >
+          <tr >
+            <td className="w-1/6 font-bold border border-separate border-green-800">Location</td>
+            {hours.map(hour =>
+              <td  key={hour} className="w-12 font-bold border border-separate border-green-800">{hour}</td>
+            )}
+            <td className="w-1/6 font-bold border border-separate border-green-800">Totals</td>
+          </tr>
+        </thead>
+        <tbody>
+
+          {props.cookiesArray.map(cookie =>
+            <tr >
+              <td key={cookie.location} className="border border-separate border-green-800 ">{cookie.location}</td>
+            </tr>
+          )}
+        </tbody>
+        <tfoot >
+          <tr >
+            <td className="font-bold border border-separate border-green-800">Total</td>
+          </tr>
+
+
+        </tfoot>
+      </table>
+      //  <p>{props.cookiesArray.map(cookie=>
+      //    <p>{`{ "Location":"${cookie.location}","minCustomers": "${cookie.minCustomer}","maxCustomers": "${cookie.minCustomer}" ,"avgCookies": "${cookie.avg}" }`}</p>
+      //    )}</p>
+
+    )
+  }
+
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import Head from 'next/head'
 import { useState } from 'react'
-
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import CreateForm from '../components/CreateForm'
+import ReportTable from '../components/RepostTable'
+import { hours } from "../data"
 export default function Home() {
 
   const [cookiesArray, setCoookiesArray] = useState([])
+  const [randCust, setRandCust] = useState([])
 
   function submitHandler(e) {
+
     e.preventDefault();
     console.log();
     const cookiesdata = {
@@ -14,90 +20,39 @@ export default function Home() {
       avg: e.target.avg.value,
       location: e.target.location.value,
     }
+    randCustPerHour()
+    console.log(cookiesArray);
     setCoookiesArray(x => [...x, cookiesdata])
-  }
+    
+    }
+    
+    function randCustPerHour() {
+    
+      if  (cookiesArray.length == 1){
+        const customer = Math.floor(Math.random()* (Math.floor(~cookiesArray[0].maxCustomer)-Math.ceil(~cookiesArray[0].minCustomer)+1)-Math.ceil(~cookiesArray[0].minCustomer))
+        console.log(customer);
+        setRandCust(x=>[...x,customer])
+      }
+      else if (cookiesArray.length >= 1) {
+      for (var i = 1; i < hours.length; i++){
+        const customer = Math.floor(Math.random()* (Math.floor(~cookiesArray[cookiesArray.length-1].maxCustomer)-Math.ceil(~cookiesArray[cookiesArray.length-1].minCustomer)+1)-Math.ceil(~cookiesArray[cookiesArray.length-1].minCustomer))
+        console.log(customer);
+        setRandCust(x=>[...x,customer])
+      }
+      }    
+     
+    }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Head>
         <title>Cookie Stand Admin</title>
       </Head>
       <Header />
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <CookieForm onSubmit={submitHandler} />
-        <p class="text-center"> Report Table Commig Soon ...</p>
-        <Result cookiesArray={cookiesArray} />
+      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
+        <CreateForm onSubmit={submitHandler} />
+        <ReportTable cookiesArray={cookiesArray} />
       </main>
-      <Footer />
+      <Footer len={cookiesArray.length} />
     </div>
   )
-}
-
-function Header() {
-  return (
-    <header class="w-full bg-green-400">
-      <h3 class="font-semibold text-black-100 md:text-3xl pt-4 pb-3 m-0"> Cookie Stand Admin</h3>
-    </header>
-  )
-}
-
-function Footer() {
-  return (
-    <footer className="flex flex-col  justify-center w-full bg-green-400 py-3">
-      <p > &copy; 2021</p>
-    </footer>
-  )
-}
-
-function CookieForm(props) {
-  return (
-    <form class="bg-green-300 bg-green-300 rounded-md" onSubmit={props.onSubmit}>
-      <div class="grid  gap-4 grid-rows-3">
-        <div class="flex flex-col pt-3 w-full  text-center font-semibold md:text-xl">Create Cookie Stand</div>
-        <div class="flex-2 m-3 ">
-          <lable class="px-4 py-2 text-black-50 font-semibold "> Location</lable>
-          <input type="text" class=" w-4/5 form-control " name="location" />
-        </div>
-        <div class="grid  gap-6 grid-cols-4 mb-5">
-          <div class="grid  gap-2 grid-rows-2 m-3 ">
-            <lable class="font-semibold"> Minimum Customers per Hour </lable>
-            <input type="text" class="form-control" name='maxCustomer' />
-          </div>
-          <div class="grid  gap-2 grid-rows-2 m-3">
-            <lable class="font-semibold" > Maximum Customers per Hour</lable>
-            <input type="text" class="form-control" name="minCustomer" />
-          </div>
-          <div class="grid  gap-2 grid-rows-2 m-3">
-            <lable class="font-semibold"> Average Cookies per Sale</lable>
-            <input type="text" class="form-control" name="avg" />
-          </div>
-          <div class="grid  gap-2 grid-rows-1 m-3">
-            <button class="font-semibold form-control bg-green-600" >Create</button>
-          </div>
-        </div>
-      </div>
-    </form>
-  )
-}
-
-function Result(props) {
- console.log(props.cookiesArray);
-  if (props.cookiesArray.length == 0) {
-    return <p>Nothing Added</p>
-  } else {
-    return (
-      <p>{props.cookiesArray.map(cookie=>
-        <p>{`{ "Location":"${cookie.location}","minCustomers": "${cookie.minCustomer}","maxCustomers": "${cookie.minCustomer}" ,"avgCookies": "${cookie.avg}" }`}</p>
-        )}</p>
-     
-      // <div class="grid  gap-4 grid-rows-3">
-      //   {props.cookiesArray.map(cookie => {
-      //     console.log(cookie);
-
-      //    
-
-      //   })}
-      // </div>)
-    )
-  }
-
 }
